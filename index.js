323c1bbacc099ed4a2e519daf1c6a27e9e788ef9
Maylon-tech <minatokiroisen77@gmail.com>
2022-03-20 14:47:45 +0900

    adding a button

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ const cardS = document.querySelector('#card1')
 botaoTurn.addEventListener('click', (e) => {
     cardS.classList.toggle('flip')
 })
+
+const botaoModal = document.querySelector('#btn')
+const caixaModal = document.querySelector('.modal-box')
+
+botaoModal.addEventListener('click', () => {
+    alert()
+    
+})
