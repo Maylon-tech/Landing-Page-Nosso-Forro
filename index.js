81e96db47ce4c8b0c821f369c8a2ce104cf09a26
Maylon-tech <minatokiroisen77@gmail.com>
2023-05-25 18:34:31 +0900

    refatorando 60%

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-const botaoTurn = document.querySelector('#turn')
-const cardS = document.querySelector('#card1')
 
-botaoTurn.addEventListener('click', (e) => {
-    cardS.classList.toggle('flip')
-})
 
-const botaoModal = document.querySelector('#btn')
-const caixaModal = document.querySelector('.modal-box')
 
-botaoModal.addEventListener('click', () => {
-    alert()
-    
-})
+const navIcon = document.querySelector('.navbar')
+
+console.log(nav.children)
